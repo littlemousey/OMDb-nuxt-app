0378d6b7dd554d12657aad5e2beee6371113fcc3
Ans de Nijs <ansdenijs@gmail.com>
2019-10-08 10:46:58 +0200

    changed api call to secure HTTPS

--- store/actions.js
+++ store/actions.js
@@ -1,7 +1,7 @@
 export default {
   async retrieveMovieData(context, movieName) {
     const moviesData = await this.$axios.$get(
-      `http://www.omdbapi.com/?s=${movieName}&apikey=16360874`
+      `https://www.omdbapi.com/?s=${movieName}&apikey=16360874`
     )
     context.commit('setMovieData', moviesData)
   }
